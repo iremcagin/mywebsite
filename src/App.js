7929e108b8bcd09506b84340b0e2dfba39f512b7
iremcagin <iremcagiin@gmail.com>
2023-03-23 09:47:30 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Home from "./components/Home.js"
 import About from "./components/About.js"
 import Projects from "./components/Projects.js"
 import {Contact} from "./components/Contact.js"
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
+import { BrowserRouter, Routes ,Route, HashRouter } from 'react-router-dom';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="about" element={<About />} />
         <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
